a1e95e07ac82ff064fba8f1f204c78f7a90a5eb5
Baliram Yadav <ballubalrambca@gmail.com>
2024-12-21 21:18:14 +0530

    1program

---
+++ FirstProgram/Helloworld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYFirstConsoleProject.FirstProgram
+{
+    internal class Helloworld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#.NET");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
